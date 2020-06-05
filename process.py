0f467c4f65c116298183fd34ced92362b3b9bdd6
carolineberger <caroline.berger2@mail.mcgill.ca>
2020-06-05 11:06:05 -0400

    make main function

--- process.py
+++ process.py
@@ -195,31 +195,34 @@ def write_csv(cleaned_data):
 
         frame.to_csv(path, index=frame_index, header=True)
 
-set_print_settings()
 
-# populate the raw data path
-raw_data_path = pathlib.Path().absolute() / gc.RAW_DATA_FOLDER_NAME
-raw_data_file_paths = []
+def main():
+    global clean_data_path
+    set_print_settings()
+    # populate the raw data path
+    raw_data_path = pathlib.Path().absolute() / gc.RAW_DATA_FOLDER_NAME
+    raw_data_file_paths = []
+    # iterate through the csv files in the raw_data folder
+    for path in raw_data_path.rglob("*.csv"):
+        raw_data_file_paths.append(path)
+    to_drop = ['Experiment', 'Schedule', 'TestName',
+               'MPoint', 'SessionName', 'SessionID', 'LaunchTime',
+               'StartTime', 'ResultTime', 'GMTOffset',
+               'Exception', 'Remark', 'blktime',
+               'blkno', 'block', 'trlno',
+               'trial', 'trlspec', 'f1',
+               'f2', 'f3', 'f4', 'msgblk_v1', 'msgblk_v2',
+               'msgblk_v4', 'msgblk_v5', 'msgblk_v7', 'msgblk_v8',
+               'lab1', 'lab2', 'lab3', 'lab4', 'lab5', 'lab6', 'lab7',
+               'lab8', 'lab9', 'lab10', 'lab11', 'lab12', 'lab13', 'lab14',
+               'lab15', 'lab16', 'lab17', 'lab18', 'lab19'
+               ]
+    clean_data_path = pathlib.Path().absolute() / gc.CLEAN_DATA_FOLDER_NAME
+    cleaned_data = clean_columns(raw_data_file_paths, to_drop)
+    write_csv(cleaned_data)
 
-# iterate through the csv files in the raw_data folder
-for path in raw_data_path.rglob("*.csv"):
-    raw_data_file_paths.append(path)
 
-to_drop = ['Experiment', 'Schedule', 'TestName',
-           'MPoint', 'SessionName', 'SessionID', 'LaunchTime',
-           'StartTime', 'ResultTime', 'GMTOffset',
-           'Exception', 'Remark', 'blktime',
-           'blkno', 'block', 'trlno',
-           'trial', 'trlspec', 'f1',
-           'f2', 'f3', 'f4', 'msgblk_v1', 'msgblk_v2',
-           'msgblk_v4', 'msgblk_v5', 'msgblk_v7', 'msgblk_v8',
-           'lab1', 'lab2', 'lab3', 'lab4', 'lab5', 'lab6', 'lab7',
-           'lab8', 'lab9', 'lab10', 'lab11', 'lab12', 'lab13', 'lab14',
-           'lab15', 'lab16', 'lab17', 'lab18', 'lab19'
-           ]
+main()
 
-clean_data_path = pathlib.Path().absolute() / gc.CLEAN_DATA_FOLDER_NAME
-
-cleaned_data = clean_columns(raw_data_file_paths, to_drop)
-
-write_csv(cleaned_data)
+if __name__ == "__main__":
+    main()
